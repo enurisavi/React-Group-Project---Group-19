ea31b6b73f1cf0248cb5985a3525e0c74d54268c
enurisavi <enurisavi2003@gmail.com>
2026-08-14 20:07:31 +0530

    feat:updated the dev

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,20 @@
-import React from 'react';
-import Navbar from './components/Navbar/Navbar';
-import './App.css';
 
-function App() {
+
 // src/App.jsx
 import React from 'react';
 import { TaskProvider } from './context/TaskContext';
+import Navbar from './components/Navbar/Navbar';
+import AddTaskForm from './components/TaskForm/AddTaskForm';
 import './App.css';
 
 export default function App() {
   return (
-    <div className="app-container">
-
-      <Navbar />
-
-      <main className="main-content">
-
-      </main>
-    </div>
-  );
-}
-
-export default App;
-
     <TaskProvider>
       <div className="app-container">
-        
+        <Navbar />
         <main className="main-content">
           {/* Member 2: <AnalyticsBar /> will go here */}
-          {/* Member 3: <AddTaskForm /> will go here */}
+          <AddTaskForm/>
           {/* Member 4: <TaskBoard /> will go here */}
         </main>
       </div>
