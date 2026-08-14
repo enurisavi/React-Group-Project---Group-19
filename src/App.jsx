df94c408294c095188fd6124c8a6e0c71584bf32
enurisavi <enurisavi2003@gmail.com>
2026-08-14 19:45:49 +0530

    feat:update the app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ export default function App() {
   return (
     <TaskProvider>
       <div className="app-container">
-        {/* Member 1: <Navbar /> will go here */}
+        
         <main className="main-content">
           {/* Member 2: <AnalyticsBar /> will go here */}
           {/* Member 3: <AddTaskForm /> will go here */}
